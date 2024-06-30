24c32ac67f65365df63f328c33c6b5c1badb4c37
vipinable <vipinable@gmail.com>
2024-06-30 13:15:00 +0200

    add backup.sh script

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#backup script for homeassistant data
+BACKUPFILE="/dev/shm/homeassiatant-$(date +%Y%m%d).tar.gz"
+cd /home/pi/homeassistant
+sudo tar -zcvf ${BACKUPFILE} esphome/config/*.yaml homebridge nlhome/config unniplaza/config
+#Upload backup to 
+scp  ${BACKUPFILE} ubuntu@www.theworkingmethods.com:/home/ubuntu/datavol/homeassistant/daily/
+#Upload backup to weekly folder as weekly backup
+if [ $(date +%u) -eq 7 ];then
+  scp ${BACKUPFILE} ubuntu@www.theworkingmethods.com:/home/ubuntu/datavol/homeassistant/weekly/
+fi
+#Upload backup to monthly folder as monthly backup
+if [ $(date +%d) -eq 1 ];then
+  scp ${BACKUPFILE} ubuntu@www.theworkingmethods.com:/home/ubuntu/datavol/homeassistant/monthly/
+fi
